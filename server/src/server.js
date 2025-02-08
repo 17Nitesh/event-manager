ae7f1f2a80b502bb754bc94d9a941e7573b3750e
Nitesh <117444692+17Nitesh@users.noreply.github.com>
2025-02-08 12:54:20 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://event-manager-pied-theta.vercel.app",
     },
 });
 
